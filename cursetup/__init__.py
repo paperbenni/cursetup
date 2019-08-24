e78c6a1b3de62194c7604c26c1119119286f634a
paperbenni <paperbenni@gmail.com>
2019-08-24 20:00:08 +0200

    centertext works

--- cursetup/__init__.py
+++ cursetup/__init__.py
@@ -20,27 +20,40 @@ def quit(stdscr):
     curses.endwin()
 
 
-def centertext(text, window):
+
+def centerheader(text, window):
     textl = text.split('\n')
     height, width = window.getmaxyx()
     textcounter = 0
     for i in textl:
-        window.addstr(height/2-len(textl) + textcounter,
-                      width/2 - (len(i)/2), text)
+        window.addstr(int(height/2 - int(len(textl)/2) + textcounter),
+                      int(width/2 - (len(i)/2)), i)
         textcounter += 1
     window.refresh()
 
 
-def animatetext(atext, window):
+def vcentertext(text, window):
+    textl = text.split('\n')
+    height, width = window.getmaxyx()
+    textcounter = 0
+    maxlen=len(max(textl, key=len))
+    for i in textl:
+        window.addstr(int(height/2 - int(len(textl)/2) + textcounter),
+                      int(width/2 - (maxlen/2)), i)
+        textcounter += 1
+    window.refresh()
+
+
+def animatetext(atext, window, textfunct):
     btext = ''
     for i in range(len(atext)):
         btext += atext[i]
-        centertext(btext, window)
+        textfunct(btext, window)
         window.refresh()
         time.sleep(.05)
 
 
-def setup_input(window):
+def chatbox(window):
     sub = window.subwin(1, 40, 3, 2)
     tb = curses.textpad.Textbox(sub)
     window.refresh()
